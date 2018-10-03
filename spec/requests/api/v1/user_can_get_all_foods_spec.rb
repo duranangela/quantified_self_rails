5ca241498c23e94495b8b1ca6b05fa54abd79fca
Angela Duran <duranangela19@gmail.com>
2018-10-03 07:16:57 -0600

    Get foods spec passes

--- spec/requests/api/v1/user_can_get_all_foods_spec.rb
+++ spec/requests/api/v1/user_can_get_all_foods_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 describe 'GET /api/v1/foods' do
   it 'can get a list of all foods in db' do
-    
+    create_list(:food, 3)
+
+    get '/api/v1/foods'
+
+    expect(response).to be_successful
+
+    foods = JSON.parse(response.body)
+
+    expect(foods.count).to eq(3)
   end
 end
