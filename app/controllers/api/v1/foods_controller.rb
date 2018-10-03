ce03a0fc1eb62b3990e5a4f9db341c906c2ad4b1
Angela Duran <duranangela19@gmail.com>
2018-10-03 13:31:17 -0600

    Add route and action for create food

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -12,4 +12,9 @@ class Api::V1::FoodsController < ApplicationController
     end
   end
 
+  def create
+    food = Food.create(name: params[:food][:name], calories: params[:food][:calories].to_i)
+    render json: food
+  end
+
 end
--- spec/requests/api/v1/user_can_create_new_food_spec.rb
+++ spec/requests/api/v1/user_can_create_new_food_spec.rb
@@ -2,17 +2,15 @@ require 'rails_helper'
 
 describe 'POST /api/v1/foods' do
   it 'can create new food' do
-    json_payload = { "food": { "name": "Apple", "calories": "220"} }.to_json
-    post "/api/v1/foods", params: json_payload
-
+    payload = { "food": { "name": "Apple", "calories": "220"} }
+    post "/api/v1/foods", params: payload
 
     expect(response).to be_successful
 
     food = JSON.parse(response.body)
 
     expect(food['id']).to eq(1)
-    expect(food['name']).to be_a('Apple')
-    expect(food['calories']).to be_a(220)
-
+    expect(food['name']).to eq('Apple')
+    expect(food['calories']).to eq(220)
   end
 end
