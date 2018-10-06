905c5077537675a376ac9fa211b0c1de6ce4cdae
Angela Duran <duranangela19@gmail.com>
2018-10-06 10:43:37 -0600

    Add sad path for mealfoods destroy

--- app/controllers/api/v1/meal_foods_controller.rb
+++ app/controllers/api/v1/meal_foods_controller.rb
@@ -12,11 +12,15 @@ class Api::V1::MealFoodsController < ApplicationController
   end
 
   def destroy
-    meal = Meal.find(params[:meal_id])
-    food = Food.find(params[:id])
-    mealfood = MealFood.where(meal_id: meal.id, food_id: food.id)
-    MealFood.delete(mealfood)
-    render json: {message: "Successfully removed #{food.name} from #{meal.name}"}
+    if Meal.exists?(params[:meal_id]) && Food.exists?(params[:id])
+      meal = Meal.find(params[:meal_id])
+      food = Food.find(params[:id])
+      mealfood = MealFood.where(meal_id: meal.id, food_id: food.id)
+      MealFood.delete(mealfood)
+      render json: {message: "Successfully removed #{food.name} from #{meal.name}"}
+    else
+      render status: 404
+    end  
   end
 
 end
