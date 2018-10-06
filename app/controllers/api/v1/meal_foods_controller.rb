9d93c90ff5af36e9d495bdcf7e78f87ebf4b80f3
Angela Duran <duranangela19@gmail.com>
2018-10-06 10:31:02 -0600

    Add sad path to mealfoods create

--- app/controllers/api/v1/meal_foods_controller.rb
+++ app/controllers/api/v1/meal_foods_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::MealFoodsController < ApplicationController
 
   def create
-    meal = Meal.find(params[:meal_id])
-    food = Food.find(params[:id])
-    mealfood = MealFood.create(meal_id: meal.id, food_id: food.id)
-    render json: {message: "Successfully added #{food.name} to #{meal.name}"}
+    if Meal.exists?(params[:meal_id]) && Food.exists?(params[:id])
+      meal = Meal.find(params[:meal_id])
+      food = Food.find(params[:id])
+      mealfood = MealFood.create(meal_id: meal.id, food_id: food.id)
+      render json: {message: "Successfully added #{food.name} to #{meal.name}"}
+    else
+      render status: 404
+    end
   end
 
 end
