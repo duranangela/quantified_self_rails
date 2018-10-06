d6dd5946311306a16da01d858970e1ea6a4b8234
Angela Duran <duranangela19@gmail.com>
2018-10-05 21:08:21 -0600

    Add validation and relationship tests for all model specs

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Food, type: :model do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:calories)}
   end
+  describe 'relationships' do
+    it {should have_many(:meal_foods)}
+    it {should have_many(:meals).through(:meal_foods)}
+  end
 end
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Meal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+  end
+  describe 'relationships' do
+    it {should have_many(:meal_foods)}
+    it {should have_many(:foods).through(:meal_foods)}
+  end
 end
