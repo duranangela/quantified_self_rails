ba4d52ccf21050c70525e245278dbe8da1694700
Angela Duran <duranangela19@gmail.com>
2018-10-05 21:09:52 -0600

    Add validations and relationships to food and meal models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,8 @@
 class Food < ApplicationRecord
 
+  has_many :meal_foods
+  has_many :meals, through: :meal_foods
+
   validates_presence_of :name, :calories
 
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,8 @@
 class Meal < ApplicationRecord
+
+  has_many :meal_foods
+  has_many :foods, through: :meal_foods
+
+  validates_presence_of :name
+  
 end
