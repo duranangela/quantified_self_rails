aa9decc28e5624bfa2eb657f68fd0d9002b92879
Angela Duran <duranangela19@gmail.com>
2018-10-06 10:28:16 -0600

    Add test for sad path for mealfoods create

--- spec/requests/api/v1/meals/user_can_add_food_to_meal_spec.rb
+++ spec/requests/api/v1/meals/user_can_add_food_to_meal_spec.rb
@@ -13,4 +13,9 @@ describe 'POST /api/v1/meals/:meal_id/foods/:id' do
 
     expect(msg['message']).to eq("Successfully added #{food.name} to #{meal.name}")
   end
+  it 'returns 404 if meal or food not found' do
+    post "/api/v1/meals/1/foods/1"
+
+    expect(response.status).to eq(404)
+  end
 end
